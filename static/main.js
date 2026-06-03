1884d71cf3886f41f20b9b003f075260835ff72c
Sixing Wang <sixingwang2025@163.com>
2026-06-03 15:33:09 +0800

    Round all four corners of avatar and card, add quotes around hitokoto text

--- static/main.js
+++ static/main.js
@@ -79,6 +79,6 @@ fetch(hitokoto_api)
   .then((data) => {
     const hitokoto = document.getElementById("hitokoto_text");
     hitokoto.href = "https://hitokoto.cn/?uuid=" + data.uuid;
-    hitokoto.innerText = data.hitokoto;
+    hitokoto.innerText = '“' + data.hitokoto + '”';
   })
   .catch(console.error);
